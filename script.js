3512c0c13ae447bb4a7f507f2f68a96c9c67e4fa
Alessandro Ferraro <ale_ferraro@libero.it>
2020-10-20 16:59:29 +0200

    Bugfix
    Implemented script that shows the discount and the final price

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ var km = prompt("Ciao! Quanti km vuoi percorrere?");
 
 var eta = prompt("Quanti anni hai?");
 
+var sconto = '';
+
+var totale = '';
+
 var prezzo = km * 0.21;
 
 if (eta < 18) {
-  prezzo = prezzo * 0.80;
+  sconto = '20%';
 } else if (eta > 65) {
-  prezzo = prezzo * 0.60;
+  sconto = '40%';
+} else {
+  sconto = 0%;
 }
 
-document.getElementById('prezzo_biglietto').innerHTML = prezzo;
+if (eta < 18) {
+  totale = prezzo * 0.80;
+} else if (eta > 65) {
+  totale = prezzo * 0.60;
+}
+
+document.getElementById('prezzo_biglietto').innerHTML = prezzo.toFixed(2);
+
+document.getElementById('sconto').innerHTML = sconto;
+
+document.getElementById('totale').innerHTML = totale.toFixed(2);
