c8ab7a2ab9dca11ca8db2c20062f135f60d202d5
Alessandro Ferraro <ale_ferraro@libero.it>
2020-10-20 18:03:00 +0200

    reduced coding rows

--- script.js
+++ script.js
@@ -10,16 +10,12 @@ var prezzo = km * 0.21;
 
 if (eta < 18) {
   sconto = '20%';
-} else if (eta > 65) {
-  sconto = '40%';
-} else {
-  sconto = '0%';
-}
-
-if (eta < 18) {
   totale = prezzo * 0.80;
 } else if (eta > 65) {
+  sconto = '40%';
   totale = prezzo * 0.60;
+} else {
+  sconto = '0%';
 }
 
 document.getElementById('prezzo_biglietto').innerHTML = prezzo.toFixed(2);
