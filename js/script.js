5c93119b6cdb6849e52aa9070b384c5438964191
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-15 16:46:48 +0100

    Add Instructions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+ il prezzo del biglietto è definito in base ai km (0.21 € al km)
+ va applicato uno sconto del 20% per i minorenni
+ va applicato uno sconto del 40% per gli over 65
+Stampare poi nella pagina il risultato.
 
+
+Partendo dall'esercizio di ieri, aggiungiamo la parte visiva come da screenshot
+Creiamo un finto biglietto del treno con:
+Nome passeggero
+Codice treno (numero casuale tra 90000 e 100000 escluso)
+Numero carrozza
+Prezzo calcolato
+Categoria selezionata dall'utente
+
+Bonus:
+Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+*/
 var COST_KM = 0.21;
 var SCONTO_GIOVANI = 0.2;
 var SCONTO_OVER = 0.4;
